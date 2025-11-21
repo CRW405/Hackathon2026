d7246f2bebc0faba5970deec2caad09237a166a3
Samantha Morales <samanthamp43@gmail.com>
2025-11-21 16:34:44 -0600

    WIP

--- swipe/cardReader.py
+++ swipe/cardReader.py
@@ -2,23 +2,26 @@ import re
 
 with open("rawInput.txt", "a") as f:
     running = True
-    pattern = r"^%B\d{17}\^[A-Z]+/[A-Z]+\^\w+\?\;\d{17}=\d+\?$"
+
+    pattern = r"^%B\d{16}\^[A-Z]+\/[A-Z]+\^\w+\?\;\d{16}=\d+\?$"
+    namePattern = r"(?<=\^)[A-Z]+\/[A-Z]+(?=\^)"
+    idPattern = r"(?<=2108701)B+\d{8}+(?=\?+\;)"
 
     while (running):
-        # cardInfo = input()
-        cardInfo = "%B6039500482024337^WIYNINGER/CALEB^2108701B00119458?;6039500482024337=2108701000119458?"
+        rawInput = input()
+        # rawInput = "%B6039500482024337^WIYNINGER/CALEB^2108701B00119458?;6039500482024337=2108701000119458?"
         
-        if "close" in cardInfo:
+        if "close" in rawInput:
             running = false
             break
-        if re.match(pattern, cardInfo):
-            name = re.search("^^$", cardInfo)
-            f.write(name)
+        if re.match(pattern, rawInput):
+            cardInfo = rawInput
+            name = re.findall(namePattern, cardInfo)
+            print(name)
             f.write("\n")
-            f.flush()
         else:
             print("NOT A VALID STUDENT ID")
-        cardInfo = ""
+        rawInput = ""
 
         
 
