cd00b25011fb2164f9b22c741f84aac0bc8bcaab
Samantha Morales <samanthamp43@gmail.com>
2025-11-21 17:03:48 -0600

    complete cardReader.py functionality

--- swipe/cardReader.py
+++ swipe/cardReader.py
@@ -1,28 +1,39 @@
 import re
+import requests
 
-with open("rawInput.txt", "a") as f:
-    running = True
+# send to localhost:6000/api/trackSwipe/<card_info> 
 
-    pattern = r"^%B\d{16}\^[A-Z]+\/[A-Z]+\^\w+\?\;\d{16}=\d+\?$"
-    namePattern = r"(?<=\^)[A-Z]+\/[A-Z]+(?=\^)"
-    idPattern = r"(?<=2108701)B+\d{8}+(?=\?+\;)"
+url = 'http://localhost:6000/api/trackSwipe/'
 
-    while (running):
-        rawInput = input()
-        # rawInput = "%B6039500482024337^WIYNINGER/CALEB^2108701B00119458?;6039500482024337=2108701000119458?"
-        
-        if "close" in rawInput:
-            running = false
-            break
-        if re.match(pattern, rawInput):
-            cardInfo = rawInput
-            name = re.findall(namePattern, cardInfo)
-            print(name)
-            f.write("\n")
-        else:
-            print("NOT A VALID STUDENT ID")
-        rawInput = ""
+# RegEx patterns to parse raw input data
 
-        
+pattern = r"^%B\d{16}\^[A-Z]+\/[A-Z]+\^\w+\?\;\d{16}=\d+\?$" # checks if input is a valid student id
+namePattern = r"(?<=\^)[A-Z]+\/[A-Z]+(?=\^)" # grabs name
+idPattern = r"(?<=2108701)B+\d{8}+(?=\?+\;)" # grabs B-Number
+    
+running = True
+while (running):
+    # rawInput = "%B6039500482024337^WIYNINGER/CALEB^2108701B00119458?;6039500482024337=2108701000119458?"
+    rawInput = input() 
+    
+    if "close" in rawInput: # closes application if close typed into input
+        running = False
+        break
+    if re.match(pattern, rawInput): # only runs if valid student ID input
+        cardInfo = rawInput
+        nameRaw = re.findall(namePattern, cardInfo)
+        names = nameRaw[0].split("/")
+        idRaw = re.findall(idPattern, cardInfo)
+
+        # FIRSTNAME, LASTNAME, B00XXXXXX
+        infoString = names[0] + ", " + names[1] + ", " + idRaw[0]
+
+        response = requests.post(url, data=infoString)
+        print(response.text)
+    else:
+        print("NOT A VALID STUDENT ID")
+    rawInput = ""
 
     
+
+
