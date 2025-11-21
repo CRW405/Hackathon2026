62fa5d8a7fcca3b00172f65a946ba6c39dea1af8
CRW405 <134819932+CRW405@users.noreply.github.com>
2025-11-21 17:40:34 -0600

    setup swipe api endpoint and mongo db

--- admin/server/routes/swipe.py
+++ admin/server/routes/swipe.py
@@ -1,11 +1,22 @@
 from flask import Blueprint, jsonify
+import datetime
+from mongodb import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["hackathon"]
+swipe_collection = db["swipes"]
 
 server = Blueprint("swipe", __name__)
 
 
-@server.route("/api/trackSwipe/<BID>", methods=["POST"])
-def on_swipe():
-    return jsonify({"status": "swipe tracked"})
+@server.route("/api/trackSwipe/<card_info>", methods=["POST"])
+def on_swipe(card_info):
+# INPUT: "LAST, FIRS BID"
+    now = datetime.datetime.now()
+    info = card_info.split(",")
+    insert = ["last": info[0].strip(), "first": info[1].strip(), "bid": info[2].strip(), "timestamp": now]
+    swipe_collection.insert_one(insert)
+
 
 
 @server.route("/api/getSwipes", methods=["POST"])
